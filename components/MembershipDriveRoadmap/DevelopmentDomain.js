0773564ed6ecbe82773509d921cef2805e86c5fd
JayKaku <jay8kaku@gmail.com>
2022-01-09 12:01:05 +0530

    Removed 'lorem ipsum' from Android Dev domain

--- components/MembershipDriveRoadmap/DevelopmentDomain.js
+++ components/MembershipDriveRoadmap/DevelopmentDomain.js
@@ -38,7 +38,7 @@ export default function DevelopmentDomain() {
       logo: "/assets/images/Android.webp",
 
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "Android is the most widely used mobile OS with more than 70% of market share.\nKotlin and Java are the primary language to build android applications",
       img_phase2: "/assets/images/Android.webp",
       phaseDetails: {
         phase1:
