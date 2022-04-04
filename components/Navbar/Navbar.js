c59224f90b0055f268aeaf561212125efeda9619
Ronak Lala <ronaklala2010@gmail.com>
2022-04-04 22:16:02 +0530

    responsive header

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg  navbar-dark bg-dark ">
-      <div className="container-fluid">
+      <div className="container-fluid homebox">
         <a className="navbar-brand" href="#">
           <Image src="/assets/images/logo.png" width={130} height={77} alt="" />{" "}
         </a>
@@ -16,15 +16,15 @@ const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className="desktop-menu navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item me-5">
               <Link href={"/"}>
-              <a className="nav-link" aria-current="page" href="#">
-                Home
-              </a>
+                <a className="nav-link" aria-current="page" href="#">
+                  Home
+                </a>
               </Link>
             </li>
             <li className="nav-item me-5 ">
