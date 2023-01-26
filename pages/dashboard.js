6feb0eff7c30e56a3bafb13ee6da88a00da3d38b
Neo945 <shreeshsrvstv@gmail.com>
2023-01-26 19:47:16 +0530

    :tada: feat: Add dashboard layout

---
+++ pages/dashboard.js
@@ -0,0 +1,48 @@
+import Head from "next/head";
+import styles from "../styles/Dashboard/Dashboard.module.css";
+
+function Element(props) {
+  return (
+    <>
+      <div className={styles.reminderElementTitle}>{props.title}</div>
+      <div className={styles.reminderElementBody}>{props.children}</div>
+    </>
+  );
+}
+
+export default function Dashboard(props) {
+  return (
+    <>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
+      <div className={styles.reminder}>
+        <div className={styles.reminderTitle}>New Reminder</div>
+        <div className={styles.reminderBody}>
+          <div className={styles.reminderBodyLeft}>
+            <Element title="Title of Reminder">
+              <input type="text" name="title" id="title" />
+            </Element>
+            <Element title="Description">
+              <textarea
+                name="decription"
+                id="decription"
+                cols="30"
+                rows="10"
+              ></textarea>
+            </Element>
+          </div>
+          <div className={styles.reminderBodyRight}>
+            <Element title="Select date and time">
+              date time selector Dropdown element
+            </Element>
+            <Element title="Add attachments">
+              <input type="file" name="attachment" id="attachment" />
+            </Element>
+            <Element title="Select students">Dropdown element</Element>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
