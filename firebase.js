1c12e42ef44fb36be30968e10585ba65b664e606
Hritik Sharma <sharmahritik2002@gmail.com>
2023-03-04 00:13:19 +0530

    Hide private data

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBRmX3Gcob7mb3ig8bIetO5lMIuz7Fr-JY",
-  authDomain: "tsec-app.firebaseapp.com",
-  databaseURL: "https://tsec-app.firebaseio.com",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: "tsec-app",
-  storageBucket: "tsec-app.appspot.com",
-  messagingSenderId: "1045382333142",
-  appId: "1:1045382333142:web:ad507705bd64d313",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
