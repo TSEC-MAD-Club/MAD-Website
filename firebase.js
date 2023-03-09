af0ffc35dcf2dff127670e17b754b3bc13b61be3
Hritik Sharma <sharmahritik2002@gmail.com>
2023-03-09 21:23:15 +0530

    Fix env variables

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
--- pages/Login.jsx
+++ pages/Login.jsx
@@ -45,19 +45,7 @@ const Login = ({ setLoggedIn }) => {
             <form
               onSubmit={(e) => {
                 e.preventDefault();
-                console.log(
-                  {
-                    apiKey: process.env.FIREBASE_API_KEY,
-                    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-                    databaseURL: process.env.FIREBASE_DATABASE_URL,
-                    projectId: process.env.FIREBASE_PROJECT_ID,
-                    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-                    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-                    appId: process.env.FIREBASE_APP_ID,
-                  },
-                  "FIREBASE_API_KEY"
-                );
-                // loginMsg();
+                loginMsg();
               }}
               className={style.rightContainer}
             >
