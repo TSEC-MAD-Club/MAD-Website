4e196046ff2a4c5effe4caf253afdc2c0fb615e9
Hritik Sharma <sharmahritik2002@gmail.com>
2023-03-09 21:42:45 +0530

    Check for env variables

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -47,13 +47,13 @@ const Login = ({ setLoggedIn }) => {
                 e.preventDefault();
                 console.log(
                   {
-                    apiKey: process.env.FIREBASE_API_KEY,
-                    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-                    databaseURL: process.env.FIREBASE_DATABASE_URL,
-                    projectId: process.env.FIREBASE_PROJECT_ID,
-                    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-                    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-                    appId: process.env.FIREBASE_APP_ID,
+                    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+                    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+                    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+                    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+                    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+                    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+                    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
                   },
                   "FIREBASE_API_KEY"
                 );
