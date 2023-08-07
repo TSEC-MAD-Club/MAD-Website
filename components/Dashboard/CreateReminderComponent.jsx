c21d7dbf9d0233f3b8a32426745e63c5a8574159
compli <shirothegamer69@gmail.com>
2023-08-07 22:38:18 +0530

    schema changed

--- components/Dashboard/CreateReminderComponent.jsx
+++ components/Dashboard/CreateReminderComponent.jsx
@@ -112,6 +112,7 @@ function CreateReminderComponent() {
       topic: notificationPath,
       sentBy: user.name,
       senderName: senderName,
+      attachments: [],
     };
     if (mediaPath) {
       upload_data.attachments = [mediaPath];
