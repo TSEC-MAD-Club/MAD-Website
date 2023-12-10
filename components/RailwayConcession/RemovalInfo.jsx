f4ed632b8279d70db9f279aba5e1d67f442664a7
Hamza Sayyed <hamzawp404@gmail.com>
2023-12-11 02:45:34 +0530

    Railway reject bug fixed

--- components/RailwayConcession/RemovalInfo.jsx
+++ components/RailwayConcession/RemovalInfo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../RailwayConcession/RailwayConcession.module.css";
 
 const RemovalInfo = ({ request, handleCloseInfoWindow }) => {
