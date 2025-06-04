8c32de7d1ee7b8bb335283893deed6582aceed82
Ayush. <awwwyushh@gmail.com>
2025-06-04 12:37:07 +0530

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function FourOFour() {
-  return <div  className="fourOfourPage"  >Page not found</div>;
+  return <div  className="fourOfourPage"  >Page not found! </div>;
 }
 
 export default FourOFour;
